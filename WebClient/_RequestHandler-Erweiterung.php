08d79ef091da196f352f1c4194a67e082ce8f324
emil-to <emil.tomasz89@gmail.com>
2018-12-13 19:47:36 +0100

    unnötige templates entfernt

--- WebClient/_RequestHandler-Erweiterung.php
+++
@@ -1,6 +0,0 @@
-    if ($autoteil->title == "") {
-      $validation_messages = array();
-      $validation_messages["title"] = "Der Titel ist eine Pflichtangabe. Bitte geben Sie einen Titel an.";
-      $response = $response->withStatus(400);
-      return $response->withJson($validation_messages);
-    }
