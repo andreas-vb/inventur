1593c5490891e2cf86e978e735c4bccae72bac57
emil-to <emil.tomasz89@gmail.com>
2018-12-13 19:53:29 +0100

    unnötige dateien in webservice gelöscht

--- WebService/_Anzahl Todos zur ID abfragen.php
+++
@@ -1,4 +0,0 @@
-$select_statement = "SELECT COUNT(*) FROM todo WHERE id = $autoteil->id";
-$result_set = $link->query($select_statement);
-$row = $result_set->fetch_row();
-$count = intval($row[0]);
--- WebService/_PUT Request-Body-Parameter.php
+++
@@ -1,5 +0,0 @@
-$request = $_REQUEST;
-if ($_SERVER["REQUEST_METHOD"] == "PUT") {
-   parse_str(file_get_contents("php://input"), $body_parameters);
-   $request = $request + $body_parameters;
-}
